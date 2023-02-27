c5cf25ca5d2cffadb8eaeeb8e43bb4fb5ff05c32
Brenda Reis <brenda.oliv.reis@hotmail.com>
2023-02-27 19:27:16 -0300

    exercício 1 do dia, concluídas as operações aritméticas

---
+++ Fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/Exercício-1-do-dia.js
@@ -0,0 +1,23 @@
+let a = 12;
+let b = 8;
+
+let adição = a + b;
+console.log(adição);
+
+let subtração = a - b;
+console.log(subtração);
+
+let multiplicação = a * b;
+console.log(multiplicação);
+
+let divisão = a / b;
+console.log(divisão);
+
+let módulo = a % b;
+console.log(módulo);
+
+if (a > b) {
+    console.log("a é maior que b");
+} else {
+    console.log("b é maior que a");
+}
