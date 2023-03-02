05361efb13dea855af15cdc25d2b4d762a428ae8
Brenda Reis <brenda.oliv.reis@hotmail.com>
2023-03-01 21:55:17 -0300

    menor número do array

--- Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/Exercício-1-do-dia.js
+++ Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/Exercício-1-do-dia.js
@@ -38,14 +38,25 @@ let result = 0;
 //console.log(higherNumber);
 
 
-for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 !== 0) {
-        result += 1;
-    }
+//for (let index = 0; index < numbers.length; index += 1) {
+//    if (numbers[index] % 2 !== 0) {
+//        result += 1;
+//    }
+//}
+
+//if (result === 0){
+//    console.log("nenhum valor ímpar encontrado");
+//} else {
+//    console.log(result);
+//}
+
+
+let smallestNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] < smallestNumber) {
+    smallestNumber = numbers[index];
+  }
 }
 
-if (result === 0){
-    console.log("nenhum valor ímpar encontrado");
-} else {
-    console.log(result);
-}
+console.log(smallestNumber);
