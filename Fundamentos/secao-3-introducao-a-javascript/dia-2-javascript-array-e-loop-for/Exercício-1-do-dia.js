4bd00375fbb528d9da82868276c8282005d446ff
Brenda Reis <brenda.oliv.reis@hotmail.com>
2023-03-01 21:36:39 -0300

    calcule e imprima a média aritmética dos valores contidos no array

--- Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/Exercício-1-do-dia.js
+++ Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/Exercício-1-do-dia.js
@@ -4,9 +4,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //  console.log(numbers[index]);
 //}
 
-let result = 0;
+
+//let result = 0;
+//for (let index = 0; index < numbers.length; index +=1) {
+//    result += numbers[index];
+//}
+
+//console.log(result);
+
+
+sum = 0;
+
 for (let index = 0; index < numbers.length; index +=1) {
-    result += numbers[index];
+    sum += numbers[index];
 }
 
-console.log(result);
+let average = sum / numbers.length;
+
+console.log(average);
